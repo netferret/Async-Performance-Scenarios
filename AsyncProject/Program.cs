19246c6d7138c4a511c75a36041f5af1c40d5aa1
Rob Tunna <netferret@gmail.com>
2020-02-09 10:34:35 +0000

    Added static using

--- AsyncProject/Program.cs
+++ AsyncProject/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace AsyncProject
 {
@@ -22,12 +23,12 @@ namespace AsyncProject
 
                 Task.WaitAll(result, result2, result3, result4);
 
-                Console.WriteLine($"Task 1 - Passed? {result.Result.Item1} - Took {result.Result.Item2} ms ");
-                Console.WriteLine($"Task 2 - Passed? {result2.Result.Item1} - Took {result2.Result.Item2} ms ");
-                Console.WriteLine($"Task 3 - Passed? {result3.Result.Item1} - Took {result3.Result.Item2} ms ");
-                Console.WriteLine($"Task 4 - Passed? {result4.Result.Item1} - Took {result4.Result.Item2} ms ");
+                WriteLine($"Task 1 - Passed? {result.Result.Item1} - Took {result.Result.Item2} ms ");
+                WriteLine($"Task 2 - Passed? {result2.Result.Item1} - Took {result2.Result.Item2} ms ");
+                WriteLine($"Task 3 - Passed? {result3.Result.Item1} - Took {result3.Result.Item2} ms ");
+                WriteLine($"Task 4 - Passed? {result4.Result.Item1} - Took {result4.Result.Item2} ms ");
                 stopwatch.Stop();
-                Console.WriteLine($"ASYNC - Completed in {stopwatch.ElapsedMilliseconds} ms" + Environment.NewLine);
+                WriteLine($"ASYNC - Completed in {stopwatch.ElapsedMilliseconds} ms" + Environment.NewLine);
             }
 
 
@@ -37,16 +38,16 @@ namespace AsyncProject
                 stopwatch.Start();
 
                 var syncResult = syncScenarios.TestStringSync();
-                Console.WriteLine($"Task 1 - Passed? {syncResult.Item1} - Took {syncResult.Item2} ms ");
+                WriteLine($"Task 1 - Passed? {syncResult.Item1} - Took {syncResult.Item2} ms ");
                 var syncResult2 = syncScenarios.TestStringSync();
-                Console.WriteLine($"Task 2 - Passed? {syncResult2.Item1} - Took {syncResult2.Item2} ms ");
+                WriteLine($"Task 2 - Passed? {syncResult2.Item1} - Took {syncResult2.Item2} ms ");
                 var syncResult3 = syncScenarios.TestStringSync();
-                Console.WriteLine($"Task 3 - Passed? {syncResult3.Item1} - Took {syncResult3.Item2} ms ");
+                WriteLine($"Task 3 - Passed? {syncResult3.Item1} - Took {syncResult3.Item2} ms ");
                 var syncResult4 = syncScenarios.TestStringSync();
-                Console.WriteLine($"Task 4 - Passed? {syncResult4.Item1} - Took {syncResult4.Item2} ms ");
+                WriteLine($"Task 4 - Passed? {syncResult4.Item1} - Took {syncResult4.Item2} ms ");
 
                 stopwatch.Stop();
-                Console.WriteLine($"SYNC - Completed in {stopwatch.ElapsedMilliseconds} ms" + Environment.NewLine);
+                WriteLine($"SYNC - Completed in {stopwatch.ElapsedMilliseconds} ms" + Environment.NewLine);
             }
 
             using (var syncScenarios = new SyncScenarios())
@@ -61,15 +62,15 @@ namespace AsyncProject
 
                 Task.WhenAll(syncTaskResult, syncTaskResult2, syncTaskResult2, syncTaskResult2);
 
-                Console.WriteLine($"Task 1 - Passed? {syncTaskResult.Result.Item1} - Took {syncTaskResult.Result.Item2} ms ");
-                Console.WriteLine($"Task 2 - Passed? {syncTaskResult2.Result.Item1} - Took {syncTaskResult2.Result.Item2} ms ");
-                Console.WriteLine($"Task 3 - Passed? {syncTaskResult3.Result.Item1} - Took {syncTaskResult3.Result.Item2} ms ");
-                Console.WriteLine($"Task 4 - Passed? {syncTaskResult4.Result.Item1} - Took {syncTaskResult4.Result.Item2} ms ");
+                WriteLine($"Task 1 - Passed? {syncTaskResult.Result.Item1} - Took {syncTaskResult.Result.Item2} ms ");
+                WriteLine($"Task 2 - Passed? {syncTaskResult2.Result.Item1} - Took {syncTaskResult2.Result.Item2} ms ");
+                WriteLine($"Task 3 - Passed? {syncTaskResult3.Result.Item1} - Took {syncTaskResult3.Result.Item2} ms ");
+                WriteLine($"Task 4 - Passed? {syncTaskResult4.Result.Item1} - Took {syncTaskResult4.Result.Item2} ms ");
 
                 stopwatch.Stop();
-                Console.WriteLine($"SYNC with Tasks - Completed in {stopwatch.ElapsedMilliseconds} ms");
+                WriteLine($"SYNC with Tasks - Completed in {stopwatch.ElapsedMilliseconds} ms");
             }
-            Console.ReadKey();
+            ReadKey();
 
         }
     }
