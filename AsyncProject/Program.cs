db36c55787cf675fcf28a3c5c2e025fb16860ca1
Robert Tunna <Robert.Tunna@it-developers.net>
2020-02-10 10:33:38 +0000

    Updated comments and parallel anti pattern

--- AsyncProject/Program.cs
+++ AsyncProject/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using static System.Console;
 
@@ -10,9 +12,12 @@ namespace AsyncProject
     {
         static async Task Main(string[] args)
         {
-            //var process = Process.GetCurrentProcess().Threads.AsParallel();
+            // Further Reading
+            // https://docs.microsoft.com/en-us/dotnet/standard/asynchronous-programming-patterns/consuming-the-task-based-asynchronous-pattern
+
             var stopwatch = new Stopwatch();
 
+            // ASYNC with Tasks
             using (var asyncScenarios = new AsyncScenarios())
             {
                 stopwatch.Start();
@@ -22,35 +27,80 @@ namespace AsyncProject
                 var result4 = Task.Run(async () => await asyncScenarios.TestStringAsync());
 
                 Task.WaitAll(result, result2, result3, result4);
+                stopwatch.Stop();
 
                 WriteLine($"Task 1 - Passed? {result.Result.Item1} - Took {result.Result.Item2} ms ");
                 WriteLine($"Task 2 - Passed? {result2.Result.Item1} - Took {result2.Result.Item2} ms ");
                 WriteLine($"Task 3 - Passed? {result3.Result.Item1} - Took {result3.Result.Item2} ms ");
                 WriteLine($"Task 4 - Passed? {result4.Result.Item1} - Took {result4.Result.Item2} ms ");
-                stopwatch.Stop();
+
+                ForegroundColor = ConsoleColor.Green;
                 WriteLine($"ASYNC with Tasks - Completed in {stopwatch.ElapsedMilliseconds} ms" + Environment.NewLine);
+                ResetColor();
             }
 
+            // ASYNC without Tasks
             using (var asyncScenarios = new AsyncScenarios())
             {
                 stopwatch.Reset();
                 stopwatch.Start();
+
                 var result = await asyncScenarios.TestStringAsync();
                 var result2 = await asyncScenarios.TestStringAsync();
                 var result3 = await asyncScenarios.TestStringAsync();
                 var result4 = await asyncScenarios.TestStringAsync();
 
                 //Task.WaitAll(result, result2, result3, result4);
+                stopwatch.Stop();
 
                 WriteLine($"Task 1 - Passed? {result.Item1} - Took {result.Item2} ms ");
                 WriteLine($"Task 2 - Passed? {result2.Item1} - Took {result2.Item2} ms ");
                 WriteLine($"Task 3 - Passed? {result3.Item1} - Took {result3.Item2} ms ");
                 WriteLine($"Task 4 - Passed? {result4.Item1} - Took {result4.Item2} ms ");
-                stopwatch.Stop();
+
+                ForegroundColor = ConsoleColor.Green;
                 WriteLine($"ASYNC without Tasks - Completed in {stopwatch.ElapsedMilliseconds} ms" + Environment.NewLine);
+                ResetColor();
             }
 
+            
+            // ASYNC without Tasks(Parallel Anti-Pattern)
+            using (var asyncScenarios = new AsyncScenarios())
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
 
+                ConcurrentBag<Task> tasks = new ConcurrentBag<Task>();
+                ParallelOptions parallelLoopOptions = new ParallelOptions() { MaxDegreeOfParallelism = 4 };
+
+                var startIndex = 0;
+                var stopIndex = 4;
+
+                Parallel.For(startIndex, stopIndex, parallelLoopOptions, i =>
+                {
+                    tasks.Add(asyncScenarios.TestStringAsync().ContinueWith((t) => { return t.Result; }));
+                });
+
+                await Task.WhenAll(tasks);
+
+                stopwatch.Stop();
+
+                foreach (Task task in tasks)
+                {
+                    //var result = task.IsCompleted;
+                    var result = task.GetType().GetProperty("Result").GetValue(task);
+                    var item1 = (((bool, long))result).Item1;
+                    var item2 = (((bool, long))result).Item2;
+                    
+                    WriteLine($"Task 1 - Passed? {item1} - Took {item2} ms ");
+                }
+
+                ForegroundColor = ConsoleColor.Green;
+                WriteLine($"ASYNC without Tasks(Parallel Anti Pattern) - Completed in {stopwatch.ElapsedMilliseconds} ms {Environment.NewLine}");
+                ResetColor();
+            }
+
+            // SYNC without Tasks
             using (var syncScenarios = new SyncScenarios())
             {
                 stopwatch.Reset();
@@ -66,9 +116,13 @@ namespace AsyncProject
                 WriteLine($"Task 4 - Passed? {syncResult4.Item1} - Took {syncResult4.Item2} ms ");
 
                 stopwatch.Stop();
+
+                ForegroundColor = ConsoleColor.Green;
                 WriteLine($"SYNC without Tasks - Completed in {stopwatch.ElapsedMilliseconds} ms" + Environment.NewLine);
+                ResetColor();
             }
 
+            // SYNC with Tasks(SYNC TO ASYNC Anti-Pattern)
             using (var syncScenarios = new SyncScenarios())
             {
                 stopwatch.Reset();
@@ -80,17 +134,19 @@ namespace AsyncProject
                 var syncTaskResult4 = Task.Run(() => syncScenarios.TestStringSync());
 
                 await Task.WhenAll(syncTaskResult, syncTaskResult2, syncTaskResult2, syncTaskResult2);
+                stopwatch.Stop();
 
                 WriteLine($"Task 1 - Passed? {syncTaskResult.Result.Item1} - Took {syncTaskResult.Result.Item2} ms ");
                 WriteLine($"Task 2 - Passed? {syncTaskResult2.Result.Item1} - Took {syncTaskResult2.Result.Item2} ms ");
                 WriteLine($"Task 3 - Passed? {syncTaskResult3.Result.Item1} - Took {syncTaskResult3.Result.Item2} ms ");
                 WriteLine($"Task 4 - Passed? {syncTaskResult4.Result.Item1} - Took {syncTaskResult4.Result.Item2} ms ");
 
-                stopwatch.Stop();
-                WriteLine($"SYNC with Tasks - Completed in {stopwatch.ElapsedMilliseconds} ms");
+                ForegroundColor = ConsoleColor.Green;
+                WriteLine($"SYNC with Tasks(SYNC TO ASYNC Anti-Pattern) - Completed in {stopwatch.ElapsedMilliseconds} ms");
+                ResetColor();
             }
-            ReadKey();
 
+            ReadKey();
         }
     }
 }
